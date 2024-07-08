4b806da457cd7b490bb8a2927be8c3e4b3862dd5
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-08 18:27:06 +0200

    feat: move route to the route lib

--- libs/fe/main/src/lib/fe-main/fe-main.config.ts
+++ libs/fe/main/src/lib/fe-main/fe-main.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { feMainRoutes } from './fe-main.routes';
+import { feRouteRoutes } from '@english-learning/fe-route';
 
 export const feMainConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(feMainRoutes)
+    provideRouter(feRouteRoutes)
   ]
 };
--- libs/fe/route/src/lib/fe-route.routes.ts
+++ libs/fe/route/src/lib/fe-route.routes.ts
@@ -1,3 +1,3 @@
 import { Route } from '@angular/router';
 
-export const feMainRoutes: Route[] = [];
+export const feRouteRoutes: Route[] = [];
