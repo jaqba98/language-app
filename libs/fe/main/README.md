2c280ffd7d8e719d00f830d86b67fd208d7f9b2c
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-08 18:06:26 +0200

    feat: create fe-main library

---
+++ libs/fe/main/README.md
@@ -0,0 +1,7 @@
+# fe-main
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test fe-main` to execute the unit tests.
---
+++ libs/fe/main/src/lib/fe-main/fe-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-fe-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fe-main.component.html',
+  styleUrl: './fe-main.component.css',
+})
+export class FeMainComponent {}
