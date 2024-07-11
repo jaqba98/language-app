ccc5f3396b0276b0e7c23cf292c28a3b8d4174cb
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-11 19:56:21 +0200

    chore: add hash

--- libs/fe/main/src/lib/fe-main/fe-main.config.ts
+++ libs/fe/main/src/lib/fe-main/fe-main.config.ts
@@ -1,13 +1,11 @@
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { feRouteRoutes } from '@english-learning/fe-route';
 
 export const feMainConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(feRouteRoutes),
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
+    provideRouter(feRouteRoutes, withHashLocation()),
   ]
 };
