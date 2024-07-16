07a5d8e1c34828f3c043be5508749fb2da90ac5a
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-16 23:24:50 +0200

    feat: add heallo world text

--- libs/fe/main/src/lib/fe-main/fe-main.component.ts
+++ libs/fe/main/src/lib/fe-main/fe-main.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MainNavComponent, MainNavOptionsType } from '@english-learning/fe-ui';
+import { CardComponent } from 'libs/fe/ui/src/lib/misc/card/card.component';
+import { TextComponent } from 'libs/fe/ui/src/lib/misc/text/text.component';
 
 @Component({
   selector: 'lib-fe-main',
   standalone: true,
   imports: [
     MainNavComponent,
+    CardComponent,
+    TextComponent,
     RouterOutlet
   ],
   templateUrl: './fe-main.component.html'
@@ -18,9 +22,6 @@ export class FeMainComponent {
     { value: 'Link 2', link: 'link2' },
     { value: 'Link 3', link: 'link3' },
     { value: 'Link 4', link: 'link4' },
-    { value: 'Link 5', link: 'link5' },
-    { value: 'Link 6', link: 'link6' },
-    { value: 'Link 7', link: 'link7' },
-    { value: 'Link 8', link: 'link8' }
+    { value: 'Link 5', link: 'link5' }
   ];
 }
