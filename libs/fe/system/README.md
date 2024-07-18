48d437891d3de237ca74dde9b03bd67318809306
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-18 11:30:31 +0200

    feat: create an empty fe-system project

---
+++ libs/fe/system/README.md
@@ -0,0 +1,7 @@
+# fe-system
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test fe-system` to execute the unit tests.
---
+++ libs/fe/system/src/lib/fe-system/fe-system.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-fe-system',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fe-system.component.html',
+  styleUrl: './fe-system.component.css',
+})
+export class FeSystemComponent {}
