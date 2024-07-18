10f8a98e01caef6138454adf8a8835fa9e07f34c
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-18 13:54:35 +0200

    refactor: libs fe main project

---
+++ libs/fe/main/src/lib/fe-main.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { FeRouteMenuOptionsService } from '@english-learning/fe-route';
+import { MainNavComponent } from '@english-learning/fe-ui';
+
+@Component({
+  selector: 'lib-fe-main',
+  standalone: true,
+  imports: [
+    MainNavComponent,
+    RouterOutlet
+  ],
+  templateUrl: './fe-main.component.html'
+})
+export class FeMainComponent extends FeRouteMenuOptionsService {
+}
--- libs/fe/route/src/lib/fe-route.routes.ts
+++ libs/fe/route/src/lib/fe-route.routes.ts
@@ -1,3 +1,17 @@
 import { Route } from '@angular/router';
 
+import { MainNavOptionsType } from "@english-learning/fe-ui";
+
 export const feRouteRoutes: Route[] = [];
+
+export class FeRouteMenuOptionsService {
+  getMenuOptions(): MainNavOptionsType {
+    return [
+      { value: 'Link 1', link: 'link1' },
+      { value: 'Link 2', link: 'link2' },
+      { value: 'Link 3', link: 'link3' },
+      { value: 'Link 4', link: 'link4' },
+      { value: 'Link 5', link: 'link5' }
+    ];
+  }
+}
