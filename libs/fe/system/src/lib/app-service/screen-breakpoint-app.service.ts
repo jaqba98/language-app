21b7582df2c7de0a4faa2fae260d4a2863f02436
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-23 10:07:02 +0200

    refactor: system project

--- libs/fe/system/src/lib/app-service/screen-breakpoint-app.service.ts
+++ libs/fe/system/src/lib/app-service/screen-breakpoint-app.service.ts
@@ -2,21 +2,20 @@ import { Injectable } from "@angular/core";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 import { BehaviorSubject } from "rxjs";
 
+import { breakpoints } from "../const/breakpoints.const";
+
 @Injectable()
 export class ScreenBreakpointAppService {
-  currentBreakpoint$ = new BehaviorSubject<string>('');
+  currentBreakpoint$ = new BehaviorSubject<string>(Breakpoints.XSmall);
   
   constructor(private readonly breakpoint: BreakpointObserver) {
-    this.breakpoint.observe([
-      Breakpoints.XSmall,
-      Breakpoints.Small,
-      Breakpoints.Medium,
-      Breakpoints.Large,
-      Breakpoints.XLarge
-    ]).subscribe(result => {
-      const breakpoints = result.breakpoints
-      const matched = Object.entries(Breakpoints)
-        .map(([key, value]) => ({ key, value, isMatched: breakpoints[value] }))
+    this.breakpoint.observe(breakpoints).subscribe(currBreakpoint => {
+      const matched = breakpoints
+        .map(([key, value]) => ({
+          key,
+          value,
+          isMatched: currBreakpoint.breakpoints[value]
+        }))
         .filter(item => item.isMatched)
         .find(item => item.key)?.value ?? '';
       this.currentBreakpoint$.next(matched);
---
+++ libs/fe/system/src/lib/const/breakpoints.const.ts
@@ -0,0 +1,9 @@
+import { Breakpoints } from "@angular/cdk/layout";
+
+export const breakpoints = [
+  Breakpoints.XSmall,
+  Breakpoints.Small,
+  Breakpoints.Medium,
+  Breakpoints.Large,
+  Breakpoints.XLarge
+];
