942d22421a56a157b909a8cca92eb41d98b0bcbe
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-24 07:57:23 +0200

    feat: create an empty fe-page project

---
+++ libs/fe/page/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/fe-page/fe-page.component';
---
+++ libs/fe/page/src/lib/fe-page/fe-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-fe-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fe-page.component.html',
+  styleUrl: './fe-page.component.css',
+})
+export class FePageComponent {}
