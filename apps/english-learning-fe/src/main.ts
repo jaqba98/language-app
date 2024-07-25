8f9ce7a83b683ae5f89509240db8bce9d67d2903
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-25 14:53:00 +0200

    refactor: english-learning-fe application

--- apps/english-learning-fe/src/main.ts
+++ apps/english-learning-fe/src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 
-import {
-  FeMainComponent,
-  feMainConfig
-} from "@english-learning/fe-main";
+import { FeMainComponent, feMainConfig } from "@english-learning/fe-main";
 
 bootstrapApplication(FeMainComponent, feMainConfig).catch(err => {
   console.error(err);
