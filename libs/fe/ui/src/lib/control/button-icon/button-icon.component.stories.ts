37bea48718b9e4344b20849d9a825f90649c1c17
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-07-30 09:44:59 +0200

    refactor: button icon component

--- libs/fe/ui/src/lib/control/button-icon/button-icon.component.stories.ts
+++ libs/fe/ui/src/lib/control/button-icon/button-icon.component.stories.ts
@@ -1,11 +1,11 @@
 import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
-
 import { ActivatedRoute } from '@angular/router';
+
 import { ButtonIconComponent } from './button-icon.component';
 
 const meta: Meta<ButtonIconComponent> = {
   component: ButtonIconComponent,
-  title: 'Control/ButtonIcon',
+  title: 'fe/ui/control/button-icon',
   decorators: [
     moduleMetadata({
       providers: [
@@ -22,5 +22,13 @@ export const Primary: Story = {
     src: 'icon/menu.svg',
     alt: 'hamburger icon',
     fullWidth: false,
+  },
+};
+
+export const FullWidth: Story = {
+  args: {
+    src: 'icon/menu.svg',
+    alt: 'hamburger icon',
+    fullWidth: true,
   },
 };
