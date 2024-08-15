013457207e8aa89571775578f1ed0fcb4960fc09
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-08-15 21:09:58 +0200

    feat: create vocabulary nav

--- libs/fe/page/src/lib/vocabulary/test1/test1.component.ts
+++ libs/fe/page/src/lib/vocabulary/test1/test1.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { RoutesMenuModel } from '@english-learning/fe-ui';
+import { RoutesMenuModel, VocabularyNavComponent } from '@english-learning/fe-ui';
+import { VocabularyComponent } from "../vocabulary.component";
 
 @Component({
   selector: 'lib-test1',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    VocabularyNavComponent,
+    VocabularyComponent
+],
   templateUrl: './test1.component.html',
 })
 export class Test1Component {
