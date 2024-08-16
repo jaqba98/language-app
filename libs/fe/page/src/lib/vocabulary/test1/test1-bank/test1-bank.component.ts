6bafbfa7cde433a4a0575bd68ff28b0ccf8b076a
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-08-16 21:18:19 +0200

    feat: create basic vocalubary bank html code

--- libs/fe/page/src/lib/vocabulary/test1/test1-bank/test1-bank.component.ts
+++ libs/fe/page/src/lib/vocabulary/test1/test1-bank/test1-bank.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
 import {
   CardComponent,
   FlexComponent,
-  FlexItemComponent,
   TextComponent,
 } from '@english-learning/fe-ui';
 
@@ -14,7 +13,6 @@ import {
   imports: [
     CommonModule,
     FlexComponent,
-    FlexItemComponent,
     CardComponent,
     TextComponent,
   ],
