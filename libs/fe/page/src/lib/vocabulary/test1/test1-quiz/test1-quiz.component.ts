1eb0cc2ab3da49a97393e8c742a570c80b192206
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-08-17 20:09:31 +0200

    feat: create basic quiz form

--- libs/fe/page/src/lib/vocabulary/test1/test1-quiz/test1-quiz.component.ts
+++ libs/fe/page/src/lib/vocabulary/test1/test1-quiz/test1-quiz.component.ts
@@ -1,58 +1,14 @@
 import { Component } from '@angular/core';
-import cloneDeep from 'clone-deep';
 
-import {
-  BankModel,
-  ButtonTextComponent,
-  CardComponent,
-  QuizComponent,
-  TextComponent,
-} from '@english-learning/fe-ui';
+import { QuizComponent } from '@english-learning/fe-ui';
 import { test1Words } from '../test1.words';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-test1-quiz',
   standalone: true,
-  imports: [
-    QuizComponent,
-    FormsModule,
-    CardComponent,
-    TextComponent,
-    ButtonTextComponent,
-  ],
+  imports: [QuizComponent],
   templateUrl: './test1-quiz.component.html',
 })
 export class Test1QuizComponent {
-  currentWord!: BankModel;
-
-  success = 0;
-
-  error = 0;
-
-  input2!: string;
-
-  words: BankModel[] = [];
-
-  constructor() {
-    this.words = cloneDeep(test1Words);
-    this.getWord();
-  }
-
-  onClick() {
-    if (this.input2 === this.currentWord.english) {
-      this.success++;
-    } else {
-      this.error++;
-    }
-    this.getWord();
-    this.input2 = "";
-  }
-
-  getWord() {
-    if (this.words.length === 0) return;
-    const word = this.words.pop();
-    if (!word) return;
-    this.currentWord = word;
-  }
+  words = test1Words;
 }
--- libs/fe/ui/src/lib/view/quiz/quiz.component.ts
+++ libs/fe/ui/src/lib/view/quiz/quiz.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import cloneDeep from 'clone-deep';
+
+import { BankModel } from '../bank/bank.model';
+import { ButtonTextComponent } from '../../control/button-text/button-text.component';
+import { CardComponent } from '../../misc/card/card.component';
+import { FlexComponent } from '../../misc/flex/flex.component';
+import { TextComponent } from '../../misc/text/text.component';
 
 @Component({
   selector: 'lib-quiz',
   standalone: true,
+  imports: [
+    CardComponent,
+    FlexComponent,
+    TextComponent,
+    ButtonTextComponent,
+  ],
   templateUrl: './quiz.component.html',
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  @Input({ required: true }) words!: BankModel[];
+
+  currentWords!: BankModel[];
+
+  currentWord!: BankModel;
+
+  ngOnInit() {
+    this.initQuiz();
+  }
+
+  onSubmit() {
+    this.nextWord();
+  }
+
+  initQuiz() {
+    this.currentWords = cloneDeep(this.words);
+    this.nextWord();
+  }
+
+  nextWord() {
+    const word = this.currentWords.pop();
+    if (!word) return;
+    this.currentWord = word;
+  }
 }
