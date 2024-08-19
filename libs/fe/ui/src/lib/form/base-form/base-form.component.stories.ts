827494a15a26e94100e4a0770efe0dc67be47949
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-08-19 08:00:43 +0200

    feat: create base form component

---
+++ libs/fe/ui/src/lib/form/base-form/base-form.component.stories.ts
@@ -0,0 +1,12 @@
+import { type Meta, type StoryObj } from '@storybook/angular';
+
+import { BaseFormComponent } from './base-form.component';
+
+const meta: Meta<BaseFormComponent> = {
+  component: BaseFormComponent,
+  title: 'fe/ui/form/base-form',
+};
+export default meta;
+type Story = StoryObj<BaseFormComponent>;
+
+export const Default: Story = {};
---
+++ libs/fe/ui/src/lib/form/base-form/base-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-base-form',
+  standalone: true,
+  templateUrl: './base-form.component.html',
+})
+export class BaseFormComponent {}
