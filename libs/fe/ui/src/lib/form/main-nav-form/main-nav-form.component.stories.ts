c6752bcd191e744ceaca2274abf0802382a667c7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-08-22 11:46:45 +0200

    feat: create mock for MockRouteNavigationService

--- libs/fe/ui/src/lib/form/main-nav-form/main-nav-form.component.stories.ts
+++ libs/fe/ui/src/lib/form/main-nav-form/main-nav-form.component.stories.ts
@@ -1,10 +1,27 @@
-import { type Meta, type StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { Injectable } from '@angular/core';
 
 import { MainNavFormComponent } from './main-nav-form.component';
+import { RouteNavigationService } from '../../infrastructure/route-navigation.service';
+
+@Injectable()
+export class MockRouteNavigationService {
+  navigate(link: string) {
+    // eslint-disable-next-line no-console
+    console.log(link);
+  }
+}
 
 const meta: Meta<MainNavFormComponent> = {
   component: MainNavFormComponent,
   title: 'fe/ui/form/main-nav-form',
+  decorators: [
+    moduleMetadata({
+      providers: [
+        { provide: RouteNavigationService, useClass: MockRouteNavigationService },
+      ],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<MainNavFormComponent>;
