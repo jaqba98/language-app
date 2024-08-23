d9e3431bcd27917b529b15bfb3b91225f68a2ab4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-08-23 13:06:36 +0200

    refactor: input component

--- libs/fe/ui/src/lib/control/input/input.component.ts
+++ libs/fe/ui/src/lib/control/input/input.component.ts
@@ -1,16 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 import { BaseControlService } from '../../form/base-form/base-control.service';
 
 @Component({
   selector: 'lib-input',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
   providers: [BaseControlService.getProvider(InputComponent)],
 })
 export class InputComponent extends BaseControlService<string> {
   @Input({ required: true }) formControl!: FormControl;
+
+  isFocused = false;
+
+  onFocus() {
+    this.isFocused = true;
+  }
+
+  onBlur() {
+    this.isFocused = false;
+  }
 }
