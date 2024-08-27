54002872f7aa30d34f465efb768af370c7d4b74e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-08-27 21:06:16 +0200

    refactor: root page component

--- libs/fe/page/src/lib/root/root.component.ts
+++ libs/fe/page/src/lib/root/root.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { MainNavComponent } from '@english-learning/fe-ui';
 
 @Component({
   selector: 'lib-root',
   standalone: true,
-  imports: [MainNavComponent],
+  imports: [
+    MainNavComponent,
+    RouterOutlet,
+  ],
   templateUrl: './root.component.html',
+  styleUrl: './root.component.scss',
 })
 export class RootComponent {}
