3a07b97d134a92c3a300d15947ff1fac421363ee
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-09-03 15:14:16 +0200

    refactor: text component

--- libs/fe/ui/src/index.ts
+++ libs/fe/ui/src/index.ts
@@ -5,8 +5,8 @@ export * from './lib/misc/card/card.component';
 export * from './lib/misc/text/text.type';
 export * from './lib/view/auth/auth.component';
 export * from './lib/control/input/input.component';
-
 export * from './lib/misc/text/text.component';
+
 export * from './lib/misc/wrapper/wrapper.component';
 export * from './lib/view/main-nav/main-nav.component';
 export * from './lib/view/grammar-template/grammar-template.component';
