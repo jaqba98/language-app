2e94f144610d40436cd69ce28cf3689f88add95f
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-09-04 13:25:23 +0200

    fix: button text component

--- libs/fe/page/.storybook/preview.ts
+++ libs/fe/page/.storybook/preview.ts
@@ -3,10 +3,14 @@ import type { Preview } from '@storybook/angular';
 const preview: Preview = {
   parameters: {
     backgrounds: {
-      default: 'default',
+      default: 'accent',
       values: [
         {
-          name: 'default',
+          name: 'primary',
+          value: '#f1f1f1',
+        },
+        {
+          name: 'accent',
           value: '#4e31aa',
         },
       ],
