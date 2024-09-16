06a9591d3d4d05abac78bb192ff8811e6a18efeb
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-09-16 18:32:41 +0200

    fix: registration storybook config

--- libs/fe/page/src/lib/registration/registration.component.stories.ts
+++ libs/fe/page/src/lib/registration/registration.component.stories.ts
@@ -5,6 +5,14 @@ import { RegistrationComponent } from './registration.component';
 const meta: Meta<RegistrationComponent> = {
   component: RegistrationComponent,
   title: 'fe/page/registration',
+  parameters: {
+    layout: 'fullscreen',
+  },
+  argTypes: {
+    event: {
+      action: 'event',
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<RegistrationComponent>;
