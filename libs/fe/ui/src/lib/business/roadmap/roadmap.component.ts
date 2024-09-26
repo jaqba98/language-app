91712532fdc8b0b38423c3b8ebdd62108e51728d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-09-26 11:40:44 +0200

    feat: create sinusoidal wave for roadmap markers

--- libs/fe/ui/src/lib/business/roadmap/roadmap.component.ts
+++ libs/fe/ui/src/lib/business/roadmap/roadmap.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RoadmapMarkerComponent } from '../roadmap-marker/roadmap-marker.component';
-import { RoadmapModel } from './roadmap.model';
+import { RoadmapMarkerViewModel, RoadmapModel } from './roadmap.model';
 import { GridComponent } from '../../layout/grid/grid.component';
 import { GridItemComponent } from '../../layout/grid-item/grid-item.component';
 
@@ -12,8 +12,40 @@ import { GridItemComponent } from '../../layout/grid-item/grid-item.component';
   imports: [CommonModule, GridComponent, GridItemComponent, RoadmapMarkerComponent],
   templateUrl: './roadmap.component.html',
 })
-export class RoadmapComponent {
+export class RoadmapComponent implements OnInit {
   @Input() model: RoadmapModel = {
     markers: [],
   };
+
+  markersView: RoadmapMarkerViewModel[] = [];
+
+  ngOnInit() {
+    let direction: 'left' | 'right' = 'left';
+    let position = 6;
+    const size = 2;
+
+    for (let i = 0; i < this.model.markers.length; i += 1) {
+      this.buildMarkerView(i, position, size);
+      if (direction === 'left') {
+        position -= size;
+      } else {
+        position += size;
+      }
+      if (position >= 12 - size) {
+        direction = 'left';
+      } else if (position <= size) {
+        direction = 'right';
+      }
+    }
+  }
+
+  private buildMarkerView(i: number, position: number, size: number) {
+    this.markersView.push({
+      ...this.model.markers[i],
+      colStart: position,
+      colEnd: position + size,
+      rowStart: i,
+      rowEnd: i + 1,
+    });
+  }
 }
