8a6b82cc8e7ef4458ab80ca6743cc023e0c9c493
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-09-26 13:20:32 +0200

    feat: make the roadmap more smooth

--- libs/fe/ui/src/lib/business/roadmap/roadmap.component.ts
+++ libs/fe/ui/src/lib/business/roadmap/roadmap.component.ts
@@ -1,51 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
-import { RoadmapMarkerComponent } from '../roadmap-marker/roadmap-marker.component';
-import { RoadmapMarkerViewModel, RoadmapModel } from './roadmap.model';
-import { GridComponent } from '../../layout/grid/grid.component';
-import { GridItemComponent } from '../../layout/grid-item/grid-item.component';
+import { RoadmapSineWaveComponent } from '../roadmap-sine-wave/roadmap-sine-wave.component';
+import { RoadmapSineWaveModel } from '../roadmap-sine-wave/roadmap-sine-wave.model';
+import { FlexComponent } from '../../layout/flex/flex.component';
 
 @Component({
   selector: 'lib-roadmap',
   standalone: true,
-  imports: [CommonModule, GridComponent, GridItemComponent, RoadmapMarkerComponent],
+  imports: [FlexComponent, RoadmapSineWaveComponent],
   templateUrl: './roadmap.component.html',
+  styleUrl: './roadmap.component.scss',
 })
-export class RoadmapComponent implements OnInit {
-  @Input() model: RoadmapModel = {
+export class RoadmapComponent {
+  @Input() model: RoadmapSineWaveModel = {
     markers: [],
   };
-
-  markersView: RoadmapMarkerViewModel[] = [];
-
-  ngOnInit() {
-    let direction: 'left' | 'right' = 'left';
-    let position = 6;
-    const size = 2;
-
-    for (let i = 0; i < this.model.markers.length; i += 1) {
-      this.buildMarkerView(i, position, size);
-      if (direction === 'left') {
-        position -= size;
-      } else {
-        position += size;
-      }
-      if (position >= 12 - size) {
-        direction = 'left';
-      } else if (position <= size) {
-        direction = 'right';
-      }
-    }
-  }
-
-  private buildMarkerView(i: number, position: number, size: number) {
-    this.markersView.push({
-      ...this.model.markers[i],
-      colStart: position,
-      colEnd: position + size,
-      rowStart: i,
-      rowEnd: i + 1,
-    });
-  }
 }
