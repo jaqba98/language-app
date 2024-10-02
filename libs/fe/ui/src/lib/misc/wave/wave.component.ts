515f7885487bd97eecaee060a95d9a7a803d4ec2
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-02 10:19:33 +0200

    feat: create an empty wave component

---
+++ libs/fe/ui/src/lib/misc/wave/wave.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-wave',
+  standalone: true,
+  templateUrl: './wave.component.html',
+  styleUrl: './wave.component.scss',
+})
+export class WaveComponent {}
