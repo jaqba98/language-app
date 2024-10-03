70a0aafe14b281f22f37b537d12d491b779f1e5e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-03 09:47:24 +0200

    refactor: course mock

--- libs/fe/mock/src/lib/store/store.mock.ts
+++ libs/fe/mock/src/lib/store/store.mock.ts
@@ -1,6 +1,6 @@
 import { provideMockStore } from '@ngrx/store/testing';
 
-import { StoreModel } from '../model/store.model';
+import { StoreModel } from '@english-learning/fe-store';
 import { courseMock } from './course.mock';
 
 export const storeMock = provideMockStore<StoreModel>({
