f62f48a60552359af1a14b91d03c54f42a4d97ab
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-04 13:56:15 +0200

    feat: create color palette

--- apps/english-learning-storybook/.storybook/preview.ts
+++ apps/english-learning-storybook/.storybook/preview.ts
@@ -3,15 +3,11 @@ import type { Preview } from '@storybook/angular';
 const preview: Preview = {
   parameters: {
     backgrounds: {
-      default: 'primary',
+      default: 'background',
       values: [
         {
-          name: 'primary',
-          value: '#f1f1f1',
-        },
-        {
-          name: 'accent',
-          value: '#4e31aa',
+          name: 'background',
+          value: '#f0eff1',
         },
       ],
     },
