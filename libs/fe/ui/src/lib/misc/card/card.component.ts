2e8cc480bd28d268c6074132cf8c756fd94ac751
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-04 09:26:35 +0200

    feat: create basic card component

--- libs/fe/ui/src/lib/misc/card/card.component.ts
+++ libs/fe/ui/src/lib/misc/card/card.component.ts
@@ -1,31 +1,37 @@
-import { Component, Input } from '@angular/core';
-import { Properties } from 'csstype';
-import { NgClass } from '@angular/common';
-
-import { CardType } from './card.type';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-card',
   standalone: true,
-  imports: [NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {
-  @Input() width: Properties['width'];
+export class CardComponent {}
 
-  @Input() type: CardType = 'default';
-
-  getCardType() {
-    switch (this.type) {
-      case 'default':
-        return 'card--default';
-      case 'main-nav':
-        return 'card--main-nav';
-      case 'main-nav-options':
-        return 'card--main-nav-options';
-      default:
-        throw new Error('Not supported card type!');
-    }
-  }
-}
+// import { Component, Input } from '@angular/core';
+// import { Properties } from 'csstype';
+// import { NgClass } from '@angular/common';
+// import { CardType } from './card.type';
+// @Component({
+//   selector: 'lib-card',
+//   standalone: true,
+//   imports: [NgClass],
+//   templateUrl: './card.component.html',
+//   styleUrl: './card.component.scss',
+// })
+// export class CardComponent {
+//   @Input() width: Properties['width'];
+//   @Input() type: CardType = 'default';
+//   getCardType() {
+//     switch (this.type) {
+//       case 'default':
+//         return 'card--default';
+//       case 'main-nav':
+//         return 'card--main-nav';
+//       case 'main-nav-options':
+//         return 'card--main-nav-options';
+//       default:
+//         throw new Error('Not supported card type!');
+//     }
+//   }
+// }
