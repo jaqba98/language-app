a2fc417bf8d00f7b3a101c525ef80c7886239688
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-07 13:16:29 +0200

    refactor: clean the input component

--- libs/fe/ui/src/lib/control/input/input.component.stories.ts
+++ libs/fe/ui/src/lib/control/input/input.component.stories.ts
@@ -1,41 +1,57 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-import { FormControl } from '@angular/forms';
 
+import { buildMeta, buildStory } from '@english-learning/fe-utils';
 import { InputComponent } from './input.component';
-import { ControlKindEnum } from '../../enum/control-kind.enum';
 
 const meta: Meta<InputComponent> = {
   component: InputComponent,
   title: 'fe/ui/control/input',
-  parameters: {
-    backgrounds: {
-      default: 'primary',
-    },
-  },
+  ...buildMeta(false),
 };
 export default meta;
 type Story = StoryObj<InputComponent>;
 
-export const Default: Story = {
-  args: {
-    form: new FormControl(''),
-    control: {
-      kind: ControlKindEnum.input,
-      id: '',
-      alignItems: 'flex-start',
-      validation: {
-        validators: [],
-        isVisible: false,
-      },
-      label: {
-        value: 'Login',
-        isVisible: true,
-      },
-      input: {
-        defaultValue: '',
-        placeholder: '',
-        type: 'text',
-      },
-    },
-  },
-};
+const buildStoryArgs = (): Story['args'] => ({});
+
+export const DefaultLight = buildStory<InputComponent>(buildStoryArgs(), true);
+
+export const DefaultDark = buildStory<InputComponent>(buildStoryArgs(), false);
+
+// import type { Meta, StoryObj } from '@storybook/angular';
+// import { FormControl } from '@angular/forms';
+// import { InputComponent } from './input.component';
+// import { ControlKindEnum } from '../../enum/control-kind.enum';
+// const meta: Meta<InputComponent> = {
+//   component: InputComponent,
+//   title: 'fe/ui/control/input',
+//   parameters: {
+//     backgrounds: {
+//       default: 'primary',
+//     },
+//   },
+// };
+// export default meta;
+// type Story = StoryObj<InputComponent>;
+// export const Default: Story = {
+//   args: {
+//     form: new FormControl(''),
+//     control: {
+//       kind: ControlKindEnum.input,
+//       id: '',
+//       alignItems: 'flex-start',
+//       validation: {
+//         validators: [],
+//         isVisible: false,
+//       },
+//       label: {
+//         value: 'Login',
+//         isVisible: true,
+//       },
+//       input: {
+//         defaultValue: '',
+//         placeholder: '',
+//         type: 'text',
+//       },
+//     },
+//   },
+// };
--- libs/fe/ui/src/lib/control/input/input.component.ts
+++ libs/fe/ui/src/lib/control/input/input.component.ts
@@ -1,36 +1,38 @@
-import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-
-import { TextComponent } from '../../misc/text/text.component';
-import { TextColorType } from '../../misc/text/text.type';
-import { ControlInputModel } from '../../model/control/control-input.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-input',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, TextComponent],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
-export class InputComponent {
-  @ViewChild('self') self!: ElementRef;
+export class InputComponent {}
 
-  @Input({ required: true }) form!: FormControl;
-
-  @Input({ required: true }) control!: ControlInputModel;
-
-  textColor: TextColorType = 'text__tertiary';
-
-  onFocus() {
-    this.textColor = 'text__accent';
-  }
-
-  onBlur() {
-    this.textColor = 'text__tertiary';
-  }
-
-  onClick() {
-    this.self.nativeElement.focus();
-  }
-}
+// import { CommonModule } from '@angular/common';
+// import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+// import { FormControl, ReactiveFormsModule } from '@angular/forms';
+// import { TextComponent } from '../../misc/text/text.component';
+// import { TextColorType } from '../../misc/text/text.type';
+// import { ControlInputModel } from '../../model/control/control-input.model';
+// @Component({
+//   selector: 'lib-input',
+//   standalone: true,
+//   imports: [CommonModule, ReactiveFormsModule, TextComponent],
+//   templateUrl: './input.component.html',
+//   styleUrl: './input.component.scss',
+// })
+// export class InputComponent {
+//   @ViewChild('self') self!: ElementRef;
+//   @Input({ required: true }) form!: FormControl;
+//   @Input({ required: true }) control!: ControlInputModel;
+//   textColor: TextColorType = 'text__tertiary';
+//   onFocus() {
+//     this.textColor = 'text__accent';
+//   }
+//   onBlur() {
+//     this.textColor = 'text__tertiary';
+//   }
+//   onClick() {
+//     this.self.nativeElement.focus();
+//   }
+// }
