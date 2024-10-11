1b4bd103632d57573336ad8ab80a3a605d1acf14
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-11 16:18:03 +0200

    feat: create basic dashboard nav

--- libs/fe/ui/src/lib/form/hamburger-form/hamburger-form.component.stories.ts
+++ libs/fe/ui/src/lib/form/hamburger-form/hamburger-form.component.stories.ts
@@ -1,17 +1,28 @@
-import { type Meta, type StoryObj } from '@storybook/angular';
+import type { Meta, StoryObj } from '@storybook/angular';
 
+import {
+  buildActivatedRouteProvider,
+  buildMetaEventAction,
+  buildMetaModuleMetaData,
+  buildStoryDarkMode,
+  buildStoryLightMode,
+} from '@english-learning/fe-utils';
 import { HamburgerFormComponent } from './hamburger-form.component';
+import { FontAwesomeComponent } from '../../external/font-awesome/font-awesome.component';
 
 const meta: Meta<HamburgerFormComponent> = {
   component: HamburgerFormComponent,
-  title: 'fe/ui/form/hamburger-form',
-  argTypes: {
-    hamburgerFormEvent: {
-      action: 'hamburgerFormEvent',
-    },
-  },
+  title: 'fe/ui/form/hamburger-nav-form',
+  ...buildMetaEventAction(),
+  ...buildMetaModuleMetaData([FontAwesomeComponent], [buildActivatedRouteProvider()]),
 };
 export default meta;
 type Story = StoryObj<HamburgerFormComponent>;
 
-export const Default: Story = {};
+export const DashboardNavFormLight: Story = {
+  ...buildStoryLightMode(),
+};
+
+export const DashboardNavFormDark: Story = {
+  ...buildStoryDarkMode(),
+};
--- libs/fe/ui/src/lib/form/hamburger-form/hamburger-form.model.ts
+++
@@ -1,3 +0,0 @@
-export interface HamburgerFormModel {
-  submit: boolean;
-}
