020810581910e2e18a019db72ce652f8be9411ed
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-11 16:18:03 +0200

    feat: create basic dashboard nav

--- libs/fe/ui/src/lib/form/hamburger-form/hamburger-form.component.ts
+++ libs/fe/ui/src/lib/form/hamburger-form/hamburger-form.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { BaseFormComponent } from '../base-form/base-form.component';
 import { BaseFormModel } from '../../model/form/base-form.model';
-import { HamburgerFormModel } from './hamburger-form.model';
 import { ControlKindEnum } from '../../enum/control-kind.enum';
+import { EventEmitterDirective } from '../../base/event-emitter.directive';
 
 @Component({
   selector: 'lib-hamburger-form',
@@ -11,15 +11,13 @@ import { ControlKindEnum } from '../../enum/control-kind.enum';
   imports: [BaseFormComponent],
   templateUrl: './hamburger-form.component.html',
 })
-export class HamburgerFormComponent {
-  @Output() hamburgerFormEvent = new EventEmitter<HamburgerFormModel>();
-
-  hamburgerForm: BaseFormModel = {
+export class HamburgerFormComponent extends EventEmitterDirective<boolean> {
+  form: BaseFormModel = {
     controls: [
       {
         kind: ControlKindEnum.buttonIcon,
         id: 'submit',
-        alignItems: 'flex-start',
+        alignItems: 'stretch',
         validation: {
           validators: [],
           isVisible: false,
@@ -31,7 +29,7 @@ export class HamburgerFormComponent {
     ],
   };
 
-  onEvent(baseForm: HamburgerFormModel) {
-    this.hamburgerFormEvent.emit(baseForm);
+  onSubmit() {
+    this.emit(true);
   }
 }
--- libs/fe/ui/src/lib/view/dashboard-nav/dashboard-nav.component.ts
+++ libs/fe/ui/src/lib/view/dashboard-nav/dashboard-nav.component.ts
@@ -11,20 +11,30 @@ import { CardComponent } from '../../misc/card/card.component';
 import { DashboardNavFormComponent } from '../../form/dashboard-nav-form/dashboard-nav-form.component';
 import { FontAwesomeComponent } from '../../external/font-awesome/font-awesome.component';
 import { FlexComponent } from '../../layout/flex/flex.component';
+import { ComponentDirective } from '../../base/component.directive';
+import { ButtonIconComponent } from '../../control/button-icon/button-icon.component';
+import { HamburgerFormComponent } from '../../form/hamburger-form/hamburger-form.component';
 
 @Component({
   selector: 'lib-dashboard-nav',
   standalone: true,
   imports: [
+    ...ComponentDirective.buildImports(),
+    ButtonIconComponent,
     CardComponent,
     FlexComponent,
     FontAwesomeComponent,
     DashboardNavFormComponent,
+    HamburgerFormComponent,
   ],
   templateUrl: './dashboard-nav.component.html',
 })
 export class DashboardNavComponent implements ObserverModel<BreakpointModel> {
-  justifyContent: Properties['justifyContent'] = 'space-around';
+  justifyContent: Properties['justifyContent'] = 'space-between';
+
+  isMobile = true;
+
+  isMenuVisible = false;
 
   constructor(private readonly breakpoint: BreakpointService) {
     this.breakpoint.addObserver(this);
@@ -32,67 +42,20 @@ export class DashboardNavComponent implements ObserverModel<BreakpointModel> {
 
   update(data: BreakpointModel) {
     const { breakpoint } = data;
+    if (breakpoint === BreakpointEnum.XSmall) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+      this.isMenuVisible = false;
+    }
     if (breakpoint === BreakpointEnum.Large || breakpoint === BreakpointEnum.XLarge) {
       this.justifyContent = 'space-around';
     } else {
       this.justifyContent = 'space-between';
     }
   }
-}
 
-// import { Component, ViewChild } from '@angular/core';
-// import { Properties } from 'csstype';
-// import { CommonModule } from '@angular/common';
-// import {
-//   ObserverModel,
-//   BreakpointModel,
-//   BreakpointService,
-//   BreakpointEnum,
-// } from '@english-learning/fe-system';
-// import { WrapperComponent } from '../../misc/wrapper/wrapper.component';
-// import { FlexComponent } from '../../layout/flex/flex.component';
-// import { CardComponent } from '../../misc/card/card.component';
-// import { IconComponent } from '../../misc/icon/icon.component';
-// import { HamburgerFormComponent } from '../../form/hamburger-form/hamburger-form.component';
-// import { MainNavFormComponent } from '../../form/main-nav-form/main-nav-form.component';
-// @Component({
-//   selector: 'lib-main-nav',
-//   standalone: true,
-//   imports: [
-//     CommonModule,
-//     FlexComponent,
-//     CardComponent,
-//     IconComponent,
-//     WrapperComponent,
-//     HamburgerFormComponent,
-//     MainNavFormComponent,
-//   ],
-//   templateUrl: './dashboard-nav.component.html',
-// })
-// export class MainNavComponent implements ObserverModel<BreakpointModel> {
-//   @ViewChild('hamburgerForm') hamburgerForm!: WrapperComponent;
-//   @ViewChild('mainNavForm') mainNavForm!: WrapperComponent;
-//   isMobile = true;
-//   isMenuVisible = false;
-//   mainNavJustifyContent: Properties['justifyContent'] = 'space-between';
-//   constructor(private readonly breakpoint: BreakpointService) {
-//     this.breakpoint.addObserver(this);
-//   }
-//   update(data: BreakpointModel) {
-//     const { breakpoint } = data;
-//     if (breakpoint === BreakpointEnum.XSmall) {
-//       this.isMobile = true;
-//     } else {
-//       this.isMobile = false;
-//       this.isMenuVisible = false;
-//     }
-//     if (breakpoint === BreakpointEnum.Large || breakpoint === BreakpointEnum.XLarge) {
-//       this.mainNavJustifyContent = 'space-around';
-//     } else {
-//       this.mainNavJustifyContent = 'space-between';
-//     }
-//   }
-//   onEvent() {
-//     this.isMenuVisible = !this.isMenuVisible;
-//   }
-// }
+  onHamburger() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+}
