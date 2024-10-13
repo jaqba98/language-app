95ac221bb83438ea3b3417135ed05261178a7133
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-13 08:40:59 +0200

    chore: turn off the base form stories

---
+++ libs/fe/ui/src/lib/form/base-form/base-form.component.stories.off.ts
@@ -0,0 +1,38 @@
+// TODO: turn on the story
+// import type { Meta, StoryObj } from '@storybook/angular';
+
+// import {
+//   buildActivatedRouteProvider,
+//   buildBaseStory,
+//   buildMetaEventAction,
+//   buildMetaModuleMetaData,
+// } from '@english-learning/fe-utils';
+// import { BaseFormComponent } from './base-form.component';
+// import { buttonIconControlStory } from '../../control/button-icon/button-icon-control-story.service';
+// import { buttonLinkControlStory } from '../../control/button-link/button-link-control-story.service';
+// import { buttonTextControlStory } from '../../control/button-text/button-text-control-story.service';
+// import { inputControlStory } from '../../control/input/input-control-story.service';
+
+// const meta: Meta<BaseFormComponent> = {
+//   component: BaseFormComponent,
+//   title: 'fe/ui/form/base-form',
+//   ...buildMetaEventAction(),
+//   ...buildMetaModuleMetaData([], [buildActivatedRouteProvider()]),
+// };
+// export default meta;
+// type Story = StoryObj<BaseFormComponent>;
+
+// const buildBaseFormArgs = (): Story['args'] => ({
+//   baseForm: {
+//     controls: {
+//       input: inputControlStory,
+//       butonText: buttonTextControlStory('submit'),
+//       buttonLink: buttonLinkControlStory,
+//       buttonIcon: buttonIconControlStory,
+//     },
+//   },
+// });
+
+// export const BaseFormLight = buildBaseStory(true, buildBaseFormArgs());
+
+// export const BaseFormDark = buildBaseStory(false, buildBaseFormArgs());
