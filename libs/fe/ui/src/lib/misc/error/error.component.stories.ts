14ecd02fe9de47905a7f3c29ec218136f1ca8d10
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-14 20:10:49 +0200

    refactor: error component

--- libs/fe/ui/src/lib/misc/error/error.component.stories.ts
+++ libs/fe/ui/src/lib/misc/error/error.component.stories.ts
@@ -1,21 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 
+import { buildBaseStory } from '@english-learning/fe-utils';
 import { ErrorComponent } from './error.component';
 
 const meta: Meta<ErrorComponent> = {
   component: ErrorComponent,
   title: 'fe/ui/misc/error',
-  parameters: {
-    backgrounds: {
-      default: 'primary',
-    },
-  },
 };
 export default meta;
 type Story = StoryObj<ErrorComponent>;
 
-export const Default: Story = {
-  args: {
-    value: 'Lorem ipsum...',
-  },
-};
+const buildErrorArgs = (): Story['args'] => ({
+  value: 'Lorem ipsum...',
+});
+
+export const ErrorLight = buildBaseStory(true, buildErrorArgs());
+export const ErrorDark = buildBaseStory(false, buildErrorArgs());
