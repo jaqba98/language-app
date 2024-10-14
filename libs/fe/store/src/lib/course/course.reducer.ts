f589c7a2b39efb41c0078185d4c6a89dab0ea5a7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-10-14 14:42:05 +0200

    feat: remove default tasks value in the store

--- libs/fe/store/src/lib/course/course.reducer.ts
+++ libs/fe/store/src/lib/course/course.reducer.ts
@@ -2,9 +2,10 @@ import { createReducer, on } from '@ngrx/store';
 
 import { CourseStoreModel } from '../model/course-store.model';
 import { getCourse } from './course.actions';
-import { courseMock } from '../mock/course.mock';
 
-const initialCourseState: CourseStoreModel = courseMock;
+const initialCourseState: CourseStoreModel = {
+  tasks: new Map(),
+};
 
 export const courseReducer = createReducer(
   initialCourseState,
